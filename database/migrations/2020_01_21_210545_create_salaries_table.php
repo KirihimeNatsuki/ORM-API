a6f678e9619dda4364eff5bb02a0e70fbd359a62
kirihime <kirihime@enzo-laravel.local>
2020-02-06 14:03:29 +0100

    Premier commit dossier ORM

---
+++ database/migrations/2020_01_21_210545_create_salaries_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalariesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('salaries', function (Blueprint $table) {
+            $table->bigIncrements('emp_no');
+            $table->string('salary');
+            $table->string('from_date');  //date
+            $table->string('to_date'); //date
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('salaries');
+    }
+}
